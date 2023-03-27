a5e9d5e3541433dff700dd06a2a67c45289bd24c
KeithSoulman1 <keithlumumba6>
2023-03-27 14:57:13 -0700

    0-reset_to_98.c

---
+++ alx-low_level_programming/0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+void reset_to_98(int *n);
+
+void swap_int(int *a, int *b);
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+void ptint__rev(char *s);
+
+void rev_string(char *s);
+
+void puts_2(char *str);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+int _atoi(char *s);
+
+#endif
